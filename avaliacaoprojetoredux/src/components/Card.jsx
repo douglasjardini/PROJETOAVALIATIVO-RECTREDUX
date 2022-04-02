29f84956e5fda2911e44e337f55e6e892bfa9b9c
douglasjardini <douglasjardini@gmail.com>
2022-04-02 15:44:23 +0000

    Avaliacao Projeto Redux - ADS2022

---
+++ avaliacaoprojetoredux/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Cards.css'
+
+function getColor(props) {
+    if(props.red) return "Red"
+    if(props.blue) return "Blue"
+    if(props.yellow) return "Yellow"
+    if(props.purlple) return "Purple"
+    if(props.green) return "Green"
+    return ""
+}
+
+export default props => {
+    
+}
